4f3f1f445aae29cea25d57363859cbb03241a1b5
bluesoc <lockhead@dev.lan>
2024-03-29 12:08:26 -0300

    Improve backend handler & Add Dynamic View

--- sim_api/api/urls.py
+++ sim_api/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('view/', views.view, name='view'),
+    path('view/<int:id>', views.view, name='view'),
     path('create/', views.create, name='create'),
     path('delete/<int:id>', views.delete, name='delete'),
     path('update/<int:id>', views.update, name='update'),
--- sim_api/api/views.py
+++ sim_api/api/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import reverse
 
 import json
@@ -16,8 +16,6 @@ from .serializer import ItemSerializer
 
 
 def index(request):
-    view_api = reverse('view')
-
     # Note that these variables are used for cosmetics only.
     # The entire "index()" method code can be deleted at will.
     # It does not affect the API.
@@ -29,61 +27,34 @@ def index(request):
         "[PUT]": "update/<int:id>",
     }
 
-    all_endpoints = ''
-
-    for method in valid_endpoints:
-        all_endpoints += f"""<tr><th>{ method }</th><th>{ valid_endpoints[method] }</th></tr>"""
-
-    index_page = f"""
-    <!-- Styling provided by W3Schools - www.w3schools.com -->
-    <style>
-        td, th {{
-          border: 1px solid #dddddf;
-          text-align: left;
-          padding: 8px;
-          font-weight: normal;
-        }}
-        tr:nth-child(even) {{
-          background-color: #dddddf;
-        }}
-    </style>
-    <h2>Django Backend API</h2>
-    <h3>Supermarket Inventory Management System</h3>
-
-    <p>View the full JSON output <a href="{view_api}">Here.</a> Or access the <a href='/admin'>Admin page</a></p>
-  
-    <p><b>Api Endpoints:</b></p>
-    <table>
-    <tr>
-        <th>Method</th>
-        <th>Route</th>
-    </tr>
-    { all_endpoints }
-    </table>
-
-    <br>
-    <p><b>Interactive API Request Interface</b></p>
-
-    <form method="POST" action="">
-        <button type="submit" name="action" value="POST">POST</button>
-        <input type="text" id="name" name="name" placeholder="Name" required size="15">
-        <input type="text" id="category" name="category" placeholder="Category" required size="10">
-        <input type="text" id="amount" name="amount" placeholder="Amount (Integer)" required size="15" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
-    </form>
-    <form method="DELETE" action="">
-        <button type="submit" name="action" value="IDK">DELETE</button>
-        <input type="text" id="id" name="id" placeholder="Id Key (Integer)" required size="15" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
-    </form>
-    <form method="PUT" action="">
-        <button type="submit" name="action" value="POST">UPDATE</button>
-    </form>
-    """
-
-    return HttpResponse(index_page)
+    return render(
+        request,
+        "api/index.html",
+        {
+            "valid_endpoints": valid_endpoints,
+        }
+    )
 
 
 @api_view(["GET"])
-def view(request):
+def view(request, id=None):
+    if id is not None:
+        try:
+            item = Item.objects.filter(pk=id)
+
+            if not item:
+                raise Exception
+
+            serialized_data = serialize("json", item)
+            return JsonResponse(
+                json.loads(serialized_data),
+                safe=False,
+                status=status.HTTP_200_OK
+                )
+        except:
+            msg = {"message": "Item not found"}
+            return JsonResponse(msg, safe=False, status=status.HTTP_404_NOT_FOUND)
+
     itens = Item.objects.all()
     serialized_data = serialize("json", itens)
     return JsonResponse(json.loads(serialized_data), safe=False)
@@ -95,9 +66,9 @@ def create(request):
 
     if serialized_item.is_valid():
         serialized_item.save()
-        return Response(serialized_item.data, status=201)
+        return Response(serialized_item.data, status=201, content_type='application/json')
 
-    return Response(serialized_item.errors, status=404)
+    return Response(serialized_item.errors, status=404, content_type='application/json')
 
 
 @api_view(['PUT'])
@@ -119,8 +90,7 @@ def delete(request, id):
     try:
         delete_item = Item.objects.get(id=id)
         delete_item.delete()
-        return Response({"message": "Item deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-    
-    except delete_item.DoesNotExist:
-        return Response({"message": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"message": "Item deleted successfully"}, status=status.HTTP_202_ACCEPTED)
 
+    except: # delete_item.DoesNotExist:
+        return Response({"message": "Item not found"}, status=status.HTTP_404_NOT_FOUND)

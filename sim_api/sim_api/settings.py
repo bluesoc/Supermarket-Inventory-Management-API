67ac45b5c7f9269f0ca60eb599579bb28c197ec3
bluesoc <145991084+bluesoc@users.noreply.github.com>
2024-08-12 15:00:28 -0300

    Use env variables

--- sim_api/sim_api/settings.py
+++ sim_api/sim_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+@q2!+94c6dxp=8=u=!0gbat7fxcp5#lwm2_gb4em84dun=%3'
+SECRET_KEY = getenv('SECRET_KEY', 'django-insecure-7+@q2!+94c6dxp=8=u=!0gbat7fxcp5#lwm2_gb4em84dun=%3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [getenv('APP_HOST', '127.0.0.1')]
 
 
 # Application definition
